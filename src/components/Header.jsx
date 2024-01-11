619cd1fae64586c845863e99834068ecb2d07a1e
Beefaz <justinas.geryba@gmail.com>
2024-01-11 14:50:01 +0200

    changed gitignore, readme, changed app to jsx format, added Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../assets/images/svg/logo.svg"
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt="kazkas"/>
+    </div>
+  )
+}
+
+export default Header;
